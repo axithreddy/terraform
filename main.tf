88ae3e7db834bfd4663b4fee32a2eee2571b7524
axith reddy mukkala <axithreddymukkala@axiths-MacBook-Air.local>
2024-11-04 12:23:04 -0800

    simple terraform setup to create vpc and subnet in aws

--- main.tf
+++ main.tf
@@ -2,10 +2,9 @@ provider "aws" {
     region = "us-east-2"
 }
 
-
 variable vpc_cidr_block {}
 variable subnet_cidr_block {}
-variable availability_zone {}
+variable avail_zone {}
 variable env_prefix {}
 
 resource "aws_vpc" "myapp-vpc" {
@@ -18,29 +17,8 @@ resource "aws_vpc" "myapp-vpc" {
 resource "aws_subnet" "myapp-subnet-1" {
   vpc_id = aws_vpc.myapp-vpc.id
   cidr_block = var.subnet_cidr_block
-  availability_zone = "us-east-2a"
+  availability_zone = var.avail_zone
   tags = {
-    Name: "subnet-1-dev"
+    Name: "${var.env_prefix}-subnet-1"
   }
 }
-
-# output "dev-vpc-id" {
-#   value = aws_vpc.development-vpc.id
-# }
-
-# output "dev-subnet-id" {
-#   value = aws_subnet.dev-subnet-1.id
-# }
-
-# data "aws_vpc" "existing-vpc" {
-#   default = true
-# }
-
-# resource "aws_subnet" "dev-subnet-2" {
-#   vpc_id = data.aws_vpc.existing-vpc.id
-#   cidr_block = "172.31.48.0/20"
-#   availability_zone = "us-east-2a"
-#   tags = {
-#     Name: "subnet-1-default"
-#   }
-# }
